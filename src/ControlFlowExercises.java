ca7229bff64fda4421b99e9b011637a86e9f5ad1
Nathaniel Gumbs <ngumbs37@gmail.com>
2019-01-08 13:54:38 -0600

    control flow do-while Square previous number starting at 2 but no larger than 1,000,000

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,6 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
+        System.out.println("Integers from 5 to 15");
         int i = 5;
         while(i <=15){
             System.out.print(i);
@@ -9,15 +10,20 @@ public class ControlFlowExercises {
             i++;
         }
         System.out.println();
+
+        System.out.println("\nCount backwards by 5's from 100 to -10");
         i = 100;
         do{
-            if (i!=-10) {
-                System.out.println(i);
-            } else {
-                System.out.print(i);
-            }
+            System.out.println(i);
             i-=5;
         }while(i >=-10);
+
+        System.out.print("\nSquare previous number starting at 2 but no larger than 1,000,000");
+        i=2;
+        do{
+            System.out.print("\n"+i);
+            i = i * i;
+        }while(i<1000000 && i != 0);
 
     }//end main
 }//end class
