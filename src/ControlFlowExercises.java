b3ee49fb7f63b903064e6149a3bcb7044e4f5683
Nathaniel Gumbs <ngumbs37@gmail.com>
2019-01-08 16:05:27 -0600

    control flow squares and cubes table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Integers from 5 to 15");
         int i = 5;
         for(; i<=15; i++){
@@ -15,27 +18,50 @@ public class ControlFlowExercises {
             System.out.println(i);
         }
 
-        System.out.print("\nSquare previous number starting at 2 but no larger than 1,000,000");
+        System.out.println("\nSquare previous number starting at 2 but no larger than 1,000,000");
         for(i = 2; i<1000000 && i != 0; i *= i){
-            System.out.print("\n"+i);
+            System.out.println(i);
         }
 
-        System.out.print("\nFizzBuzz");
+        System.out.println("\nFizzBuzz 3 and 5");
 
         for(i=1; i <= 100; i++){
-            System.out.println();
+
             if(i % 3 == 0){
                 System.out.print("Fizz");
             }
             if(i % 5 == 0){
                 System.out.print("Buzz");
-            } else{
+            } else {
                 if (i % 3 != 0 && i % 5 != 0){
                     System.out.print(i);
                 }
             }
-
+            System.out.println();
         }
+
+        System.out.println("The system will generate squares and cubes up to a given integer.");
+        System.out.println("What number would you like to go up to?");
+        int x = Integer.parseInt(scanner.nextLine());
+
+        String answer;
+        boolean keepGoing;
+
+        do{
+            System.out.println("| number | squared | cubed");
+            System.out.println("| ------ | ------- | -----");
+
+            for(i = 1; i<= x; i++){
+                System.out.printf("| %-6d | %-7d | %-5d\n", i, (i*i),(i*i*i));
+            }
+            System.out.println("Generate another table?");
+            answer = scanner.nextLine();
+            keepGoing = answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("y");
+            if(keepGoing){
+                System.out.println("What number would you like to go up to?");
+                x = Integer.parseInt(scanner.nextLine());
+            }
+        }while(keepGoing);
 
     }//end main
 }//end class
