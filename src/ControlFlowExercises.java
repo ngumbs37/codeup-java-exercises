df417a0889690363314e80a8e68deacb478560a0
Nathaniel Gumbs <ngumbs37@gmail.com>
2019-01-08 13:59:59 -0600

    control flow do-while refactor to for loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,28 +2,23 @@ public class ControlFlowExercises {
     public static void main(String[] args){
         System.out.println("Integers from 5 to 15");
         int i = 5;
-        while(i <=15){
+        for(; i<=15; i++){
             System.out.print(i);
             if (i <15){
-                    System.out.print(" ");
-                }
-            i++;
+                System.out.print(" ");
+            }
         }
         System.out.println();
 
         System.out.println("\nCount backwards by 5's from 100 to -10");
-        i = 100;
-        do{
+        for(i = 100;i >=-10;i-=5){
             System.out.println(i);
-            i-=5;
-        }while(i >=-10);
+        }
 
         System.out.print("\nSquare previous number starting at 2 but no larger than 1,000,000");
-        i=2;
-        do{
+        for(i = 2; i<1000000 && i != 0; i *= i){
             System.out.print("\n"+i);
-            i = i * i;
-        }while(i<1000000 && i != 0);
+        }
 
     }//end main
 }//end class
