a12494bf3144bb279b46e1794a923334fbcdc70c
Nathaniel Gumbs <ngumbs37@gmail.com>
2019-01-08 12:17:36 -0600

    control flow exercises

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,13 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+        int i = 5;
+        while(i <=15){
+            System.out.print(i);
+            if (i <15){
+                    System.out.print(" ");
+                }
+            i++;
+        }
+
+    }//end main
+}//end class
